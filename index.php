ca00271521bc52358dd77609b6cf032e67511737
FELIPE ASSIS DE JESUS <45512256+lipeA@users.noreply.github.com>
2020-11-23 23:01:26 -0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Adicionando fonts google -->
+    <link href = "https://fonts.googleapis.com/css2? family = Roboto: wght @ 100 & display = swap" rel = "stylesheet">
+    <title>thema danki</title>
+</head>
+<body>
+    <!-- Area do cabeçalho (menu) -->
+   <section class="topo"> 
+    <!-- classe para centralizar o conteudo -->  
+        <div class="center">  
+                <header class="logo">
+                    <img src="img/logo.png" alt="">
+                </header>
+    <!-- Botões do menu -->
+                <ul class="menu-desktop">
+                    <li> <a href="http://">Home</a></li>
+                    <li> <a href="http://">Sobre</a></li>
+                    <li> <a href="http://">Contato</a></li>
+                </ul>
+                <div class="clear"></div>
+        </div>
+    </section>
+</body>
+</html>
