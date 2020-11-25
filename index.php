fa1bd705b3824d0069ceb1f5b29eb16c4e5f75be
FELIPE ASSIS DE JESUS <45512256+lipeA@users.noreply.github.com>
2020-11-25 16:51:57 -0300

    mudanças

--- index.php
+++ index.php
@@ -11,18 +11,20 @@
 <body>
     <!-- Area do cabeçalho (menu) -->
    <section class="topo"> 
-    <!-- classe para centralizar o conteudo -->  
+         <!-- classe para centralizar o conteudo -->  
         <div class="center">  
-                <header class="logo">
-                    <img src="img/logo.png" alt="">
-                </header>
-    <!-- Botões do menu -->
-                <ul class="menu-desktop">
-                    <li> <a href="http://">Home</a></li>
-                    <li> <a href="http://">Sobre</a></li>
-                    <li> <a href="http://">Contato</a></li>
-                </ul>
-                <div class="clear"></div>
+                  <header class="logo"> <img src="img/logo.png" alt=""></header>
+                <!-- Botões do menu -->
+                   <ul class="menu-desktop">
+                       <li> <a href="http://">Home</a></li>
+                       <li> <a href="http://">Sobre</a></li>
+                       <li> <a href="http://">Contato</a></li>
+                   </ul>
+                   <div class="clear"></div>
+                <!-- conteudo no cabeçalho -->
+                   <div class="conteudo-topo">
+                       <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ullam dolor delectus fugit asperiores nesciunt maiores harum vero perferendis quasi facilis officia, labore eligendi nisi, corporis recusandae ea reiciendis consequatur omnis.</p>
+                   </div>    
         </div>
     </section>
 </body>
